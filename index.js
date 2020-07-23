77a7bc265f83cf4026c39468f919b2a8248f596a
devih2001 <46088897+devih2001@users.noreply.github.com>
2020-07-23 10:30:51 +0700

    update
    delete count.js (react facebook)  and edit index.js

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
 const express = require('express');
-const app = express();
 const request = require('request');
 const cheerio = require('cheerio');
-//const fs = require('fs');
+const app = express();
+
 
 app.use(express.static("public"));
-// app.set("view engine", "ejs");
 app.set("views", "./views");
 app.listen(9080);
 
 var obj ={};
 
 function crawlData(){
-    app.get("/masv", function (req, res) {
-        request("http://congthongtin.hvnh.edu.vn/default.aspx?page=thoikhoabieu&sta=1&id=22A4010518", function (error, response, body) {
+    app.get("/", function (req, res) {
+        request("http://qldt.ptit.edu.vn/default.aspx?page=thoikhoabieu&sta=1&id=B19DCCN719", function (error, response, body) {
             if (error) {
                 console.log(error);
                 res.status(302).send({ error: "Couldnt connect to qldt because url not found" })
 
             }
             else {
-                //console.log(body);
                 $ = cheerio.load(body);
                 var maSV = $(body).find("span#ctl00_ContentPlaceHolder1_ctl00_lblContentMaSV");
                 var nameAndBirthday = $(body).find("span#ctl00_ContentPlaceHolder1_ctl00_lblContentTenSV");
                 var classAndBrand = $(body).find("span#ctl00_ContentPlaceHolder1_ctl00_lblContentLopSV");
 
-                //console.log(list);
-                //fs.writeFileSync('./data.json',JSON.stringify(list, null, 2));
+                
                 obj.masinhvien = maSV.text();
                 obj.hoten = nameAndBirthday.text();
                 obj.lop = classAndBrand.text();
@@ -65,7 +62,7 @@ function crawlData(){
                 obj.lop = obj.lop.slice(0, checkBrand + 2);
                 obj.ngaysinh = stringBirthday;
                 res.status(200).send(obj);     
-                console.log(obj);
+                
 
             }
 
